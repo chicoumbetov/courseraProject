257638c5b12ddae40c8b9ebf5ddfeaecb395c048
chicoumbetov <shyngys-maaan@mail.ru>
2020-12-07 04:32:19 +0600

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
-    <div >
-      <MainComponent />
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <h1>Hey</h1>
     </div>
   );
 }
