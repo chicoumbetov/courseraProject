8a9062d9132c638c72e7b6de465a905a34ee1bed
chicoumbetov <shyngys-maaan@mail.ru>
2020-12-09 03:14:55 +0600

    React Route/ refactor onClick

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import { DISHES } from "../shared/dishes";
 import Footer from "./FooterComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 
 class Main extends Component {
@@ -22,17 +24,27 @@ class Main extends Component {
     }
 
     render() {
+
+        const HomePage = () => {
+            return(
+                <Home/>
+            )
+        }
+
         return (
 
             <div >
-                <Header/>
-                <Menu dishes={this.state.dishes} 
-                      onClick={(dishId) => this.onDishSelect(dishId)}
+                <Header />
+                <Switch>
+                    < Route path="/home" component={HomePage} />
+                    < Route exact path="/menu" component={() => < Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
+
+                <DishDetail
+                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
                 />
-                <DishDetail 
-                    dish={ this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} 
-                />
-                <Footer/>
+                <Footer />
 
             </div>
 
