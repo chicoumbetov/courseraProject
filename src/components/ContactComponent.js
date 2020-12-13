17eadb3ac31809f2f3f317faebcdd710ea41ce86
chicoumbetov <shyngys-maaan@mail.ru>
2020-12-13 18:42:41 +0600

    single page application, path, link replacing of onclick

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function Contact(props) {
+    return (
+        <div className="container">
+
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to='/home'>Home</Link>
+                    </BreadcrumbItem>
+
+                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    <div className="col-12">
+                        <h3>Contact Us</h3>
+                        <hr />
+                    </div>
+                </Breadcrumb>
+            </div>
+
+            <div className="row row-content">
+                <div className="col-12">
+                    <h3>Location Information</h3>
+                </div>
+                <div className="col-12 col-sm-4 offset-sm-1">
+                    <h5>Our Address</h5>
+                    <address>
+                        121, Clear Water Bay Road<br />
+                        Clear Water Bay, Kowloon<br />
+                        FRANCE<br />
+                        <i className="fa fa-phone"></i>: +33 07 66 198985<br />
+                        <i className="fa fa-fax"></i>: +123456789<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                    </address>
+                </div>
+                <div className="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                </div>
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary" href='tel: +77073156879' ><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info" href='skype: chingiz' ><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
@@ -14,7 +18,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         }
     }
 
@@ -26,8 +32,19 @@ class Main extends Component {
     render() {
 
         const HomePage = () => {
-            return(
-                <Home/>
+            return (
+                < Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                       promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                       leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            )
+        }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                />
             )
         }
 
@@ -37,8 +54,12 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     < Route path="/home" component={HomePage} />
+
                     < Route exact path="/menu" component={() => < Menu dishes={this.state.dishes} />} />
-                    <Redirect to="/home" />
+                    < Route path="/menu/:dishId" component={DishWithId} />
+
+                    < Route exact path="/contactus" component={Contact} />
+                    < Redirect to="/home" />
                 </Switch>
 
                 <DishDetail
